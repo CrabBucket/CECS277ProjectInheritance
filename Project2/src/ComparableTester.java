dd2787d6d711374cc82841d08777db3362bf30f0
CrabBucket <tom@mcratracing.com>
2019-09-25 15:26:11 -0700

    Gave the tester a main method again for testing.  Added more desserts

--- Project2/src/ComparableTester.java
+++ Project2/src/ComparableTester.java
@@ -2,13 +2,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ComparableTester {
-
+	public static void main(String[] args) {
+		test();
+	}
 	public static void test() {
 		ArrayList<DessertItem> list = new ArrayList<DessertItem>();
-		list.add(new Candy("Most Expensive", 32, 10, 50000));
+		
 		list.add(new Icecream("Cheapest", 32, 1));
 		list.add(new Sundae("2",32,1,1));
-		list.add(new Cookie("3", 32, 10, 500));
+		list.add(new Sundae("3",32,1,2));
+		list.add(new Icecream("4", 32, 15));
+		list.add(new Cookie("5", 32, 10, 500));
+		list.add(new Cookie("6", 32, 10, 510));
+		list.add(new Candy("Most Expensive", 32, 10, 50000));
 		Collections.shuffle(list);
 		// Candy() should have cost 0 so any other DessertItem should be larger
 		DessertItem expensive = new Candy();
