44f7e52322cff8d10e08ac9428c837849e613a35
CrabBucket <tom@mcratracing.com>
2019-09-26 10:30:29 -0700

    Added polymorphic tester for more explicit testing.

---
+++ Project2/src/PolymorphicTester.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class PolymorphicTester {
+
+	public static void main(String[] args) {
+		ArrayList<DessertItem> listI = new ArrayList<DessertItem>();
+		ArrayList<DessertItem> listC = new ArrayList<DessertItem>();
+		ArrayList<DessertItem> listCndy = new ArrayList<DessertItem>();
+		ArrayList<DessertItem> listS = new ArrayList<DessertItem>();
+		ArrayList<ArrayList<DessertItem>> master = new ArrayList<ArrayList<DessertItem>>();
+		master.add(listI);
+		master.add(listC);
+		master.add(listCndy);
+		master.add(listS);
+		
+		
+		
+		listI.add(new Icecream("Cheapest", 32, 1));
+		listS.add(new Sundae("2",32,1,1));
+		listS.add(new Sundae("3",32,1,2));
+		listI.add(new Icecream("4", 32, 15));
+		listC.add(new Cookie("5", 32, 10, 500));
+		listC.add(new Cookie("6", 32, 10, 510));
+		listCndy.add(new Candy("7", 32, 10, 50000));
+		listCndy.add(new Candy("Most Expensive", 32, 10, 50000));
+		
+		for(ArrayList<DessertItem> list:master) {
+			for(DessertItem d: list) {
+				System.out.println(d.getName() + " compared to:");
+				for(ArrayList<DessertItem> sub:master) {
+					
+					for(DessertItem e: sub) {
+						System.out.println(e.getName()+ " evaluates to: " + d.compareTo(e));
+					}
+				}
+				System.out.println();
+			
+			}
+		}
+		
+
+	}
+
+}
